263b99d861f812551b401c734d4ca220d1679476
pyraxo <pyraxo@tfwno.gf>
2017-08-02 19:57:50 +0800

    Fix command resolvers

--- src/resolvers/command.js
+++ src/resolvers/command.js
@@ -1,7 +1,7 @@
 module.exports = {
   type: 'command',
-  resolve: (content, args, msg, { engine }) => {
-    const command = engine.commands.get(content.toLowerCase())
+  resolve: (content, args, msg, { commands }) => {
+    const command = commands.get(content.toLowerCase())
     return !command ||
     ((command.options || {}).adminOnly && !process.env.ADMIN_IDS.split(', ').includes(msg.author.id))
     ? Promise.reject('command.NOT_FOUND')
--- src/resolvers/commandGroup.js
+++ src/resolvers/commandGroup.js
@@ -1,7 +1,7 @@
 module.exports = {
   type: 'commandGroup',
-  resolve: (content, args, msg, { engine }) => {
-    const group = engine.commands.find(c => c.group === content.toLowerCase())
+  resolve: (content, args, msg, { commands }) => {
+    const group = commands.find(c => c.group === content.toLowerCase())
     return group ? Promise.resolve(group) : Promise.reject('group.NOT_FOUND')
   }
 }
